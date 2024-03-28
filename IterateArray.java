33f038daaf9fae34f8853059082890aa561c726a
AzizKhan2004 <152771333+AzizKhan2004@users.noreply.github.com>
2024-03-28 12:32:30 +0530

    Add files via upload

---
+++ IterateArray.java
@@ -0,0 +1,11 @@
+class IterateArr{
+    public static void main(String[] ar){
+        int[] arr={1,2,4,5,6,7};
+        /*ComArray.main(null) */
+        System.out.println(1);
+        System.out.println("elements are");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
